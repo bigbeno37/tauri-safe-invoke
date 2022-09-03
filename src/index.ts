1f6b04a8ffe5ba894c590ab3d3ed6d7dba0296e5
Ben O'Sullivan <contact@benosullivan.com>
2022-09-03 23:23:43 +1000

    Modified API of createSafeInvoker to always return a SafeInvokeResult to account for potential promise errors

--- src/index.ts
+++ src/index.ts
@@ -1,54 +1,101 @@
 import {invoke} from '@tauri-apps/api';
-import {z} from "zod";
+import {z, ZodSchema} from 'zod';
+import { InvokeArgs } from '@tauri-apps/api/tauri';
 
+/**
+ * Represents validation for a Result returned from invocation of a command.
+ */
 type ResultSchema<R extends z.ZodSchema, E extends z.ZodSchema> = {
+	/**
+	 * The schema for Ok results.
+	 */
 	ok: R;
+
+	/**
+	 * The schema for Err results.
+	 */
 	err: E;
 };
 
+/**
+ * Configuration for whether safe invocations should throw errors.
+ */
 type ThrowParseError<T extends boolean> = {
 	throwParseError: T;
 }
 
+/**
+ * Represents the result of a safe invocation.
+ */
 type SafeInvokeResult<D, E> = { ok: true, data: D } | { ok: false, error: E };
-type SafeParse<T extends z.ZodSchema> = ReturnType<T["safeParse"]>;
 
-type SafeInvoke<T> = <U extends Record<string, unknown> | void>(data: U) => Promise<T>;
-
-type CreateSafeInvoker = {
-	<R extends z.ZodSchema>(command: string, responseSchema: R, config?: ThrowParseError<true>): SafeInvoke<SafeInvokeResult<z.infer<R>, unknown>>;
-	<R extends z.ZodSchema>(command: string, responseSchema: R, config: ThrowParseError<false>): SafeInvoke<SafeInvokeResult<SafeParse<R>, unknown>>;
-	<R extends z.ZodSchema, E extends z.ZodSchema>(command: string, resultSchema: ResultSchema<R, E>, config?: ThrowParseError<true>): SafeInvoke<SafeInvokeResult<z.infer<R>, z.infer<E>>>;
-	<R extends z.ZodSchema, E extends z.ZodSchema>(command: string, resultSchema: ResultSchema<R, E>, config: ThrowParseError<false>): SafeInvoke<SafeInvokeResult<SafeParse<R>, SafeParse<E>>>;
-};
+/**
+ * Utility type to return the result of a safeParse call on a Zod schema.
+ */
+type SafeParse<T extends z.ZodSchema> = ReturnType<T['safeParse']>;
 
 const isResultSchema = <R extends z.ZodSchema, E extends z.ZodSchema>(schema: z.ZodSchema | ResultSchema<R, E>): schema is ResultSchema<R, E> => {
 	return !!((schema as ResultSchema<R, E>).ok && (schema as ResultSchema<R, E>).err);
 };
 
-export const createSafeInvoker: CreateSafeInvoker = <R extends z.ZodSchema, E extends z.ZodSchema>(command: string, schema: R | ResultSchema<R, E>, config: ThrowParseError<boolean> | undefined): SafeInvoke<SafeInvokeResult<R, unknown>> | SafeInvoke<SafeInvokeResult<SafeParse<R>, unknown>> | SafeInvoke<SafeInvokeResult<R, E>> | SafeInvoke<SafeInvokeResult<SafeParse<R>, SafeParse<E>>> =>
-	async data => {
-		let validationResult;
+/**
+ * Returns a function that, when called, invokes the given command in the Tauri backend. When a response is received
+ * from the invocation, it will be compared against the given schema and, by default, will throw an Error if the
+ * response doesn't match the given schema.
+ *
+ * @param command The command to be invoked.
+ * @param schema The Zod schema(s) to use when checking the response of an invocation.
+ * @param config Configuration for the generated invocation function.
+ */
+export const createSafeInvoker = <S extends ZodSchema | ResultSchema<ZodSchema, ZodSchema>, C extends ThrowParseError<boolean>>
+	(command: string, schema: S, config?: C) => async <T extends Record<string, unknown> | void>(data: T): Promise<
+		S extends ZodSchema
+			? C extends ThrowParseError<false>
+				? SafeInvokeResult<SafeParse<S>, unknown>
+				: SafeInvokeResult<z.infer<S>, unknown>
+			: S extends ResultSchema<infer R, infer E>
+				? C extends ThrowParseError<false>
+					? SafeInvokeResult<SafeParse<R>, SafeParse<E>>
+					: SafeInvokeResult<z.infer<R>, z.infer<E>>
+				: never
+	> => {
+
+		let parseResult;
 
 		try {
-			const response = invoke(command, data as Record<string, unknown>);
+			const response = invoke(command, data as InvokeArgs);
 
-			validationResult = isResultSchema(schema) ? schema.ok.safeParse(response) : schema.safeParse(response);
+			parseResult = isResultSchema(schema) ? schema.ok.safeParse(response) : schema.safeParse(response);
+
+			if (config && !config.throwParseError) {
+			// TODO: Remove any
+				return { ok: true, data: parseResult } as any;
+			}
 		} catch (e) {
 			if (!isResultSchema(schema)) {
-				return { ok: false, error: e };
+			// TODO: Remove any
+				return { ok: false, error: e } as any;
 			}
 
-			validationResult = schema.err.safeParse(e);
+			const result = schema.err.safeParse(e);
+
+			if (config && !config.throwParseError) {
+			// TODO: Remove any
+				return { ok: false, error: result } as any;
+			}
+
+			if (!result.success) {
+				throw new Error(`Invalid error type received while invoking command "${command}"! Cause: ${result.error.format()}`);
+			}
+
+			// TODO: Remove any
+			return { ok: false, data: result.data } as any;
 		}
 
-		if (config && !config.throwParseError) {
-			return {ok: true, data: validationResult};
+		if (!parseResult.success) {
+			throw new Error(`Invalid response received while invoking command "${command}"! Cause: ${parseResult.error.format()}`);
 		}
 
-		if (!validationResult.success) {
-			throw new Error("Response from invoked command did not match expected schema! Cause:" + validationResult.error.format());
-		}
-
-		return { ok: true, data: validationResult.data };
+		// TODO: Remove any
+		return { ok: true, data: parseResult.data } as any;
 	};
